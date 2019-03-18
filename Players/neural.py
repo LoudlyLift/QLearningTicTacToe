776dace64f0b88bb500d9ed3a734b993981212ce
Ivan Johnson <git@IvanJohnson.net>
2019-03-18 11:27:44 -0500

    Increase threshold at which huber becomes linear

--- Players/neural.py
+++ Players/neural.py
@@ -44,7 +44,7 @@ class neural:
                 self._computedQ = net
                 self._targetQ = tf.placeholder(shape=[None, num_actions],dtype=tf.float32, name="targetQ")
 
-                self._loss = tf.losses.huber_loss(self._targetQ, self._computedQ)
+                self._loss = tf.losses.huber_loss(self._targetQ, self._computedQ, delta=10.0)
                 trainer = tf.train.GradientDescentOptimizer(learning_rate=self._config["learning_rate"])
                 self._updateModel = trainer.minimize(self._loss)
 
